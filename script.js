663ae03659c6e4e201a6ff883443a93f447a673f
elgfe <170134446+slothheadmuffin@users.noreply.github.com>
2024-07-10 19:07:16 -0700

    Started adding functioning to buttons

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('hello');
+const numeros=document.querySelector("#eight");
+const resultado=document.querySelector("#result");
+numeros.addEventListener("click",()=>{
+    console.log(numeros.textContent);
+    resultado.textContent=numeros.textContent;
+})
