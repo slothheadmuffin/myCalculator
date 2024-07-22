b153d67c37b63d437f168012216479d19a818de9
elgfe <170134446+slothheadmuffin@users.noreply.github.com>
2024-07-22 15:50:39 -0700

    fixed the normal delete button

--- script.js
+++ script.js
@@ -8,60 +8,85 @@ const restar=document.querySelector("#restar");
 const multiplicar=document.querySelector("#multiplicar");
 const dividir=document.querySelector("#dividir");
 
-
-
-
-
 //Funciona con botones
 numeros.addEventListener("click",(e)=>{
     switch(e.target.id){
+        //numeros y punto
         case 'one':
             resultado.textContent+='1';
-            console.log("hi");
+
             break;
         case 'two':
             resultado.textContent+='2';
-            console.log("hi");
+
             break;
         case 'three':
             resultado.textContent+='3';
-            console.log("hi");
+
             break;
         case 'four':
             resultado.textContent+='4';
-            console.log("hi");
+
             break;
         case 'five':
             resultado.textContent+='5';
-            console.log("hi");
+
             break;
         case 'six':
             resultado.textContent+='6';
-            console.log("hi");
+
             break;
         case 'seven':
             resultado.textContent+='7';
-            console.log("hi");
+
             break;
         case 'eight':
             resultado.textContent+='8';
-            console.log("hi");
+
             break;
         case 'nine':
         resultado.textContent+='9';
-        console.log("hi");
             break;
         case 'zero':
             resultado.textContent+='0';
-            console.log("hi");
+
             break;
         case 'dot':
             const texto=resultado.textContent;
             textArr=texto.split('').filter(item => item.includes('.'));
-            console.log(textArr);
+            
             if (textArr.length>0){
                alert("Solo puedes escribir un punto"); 
             }else{resultado.textContent+='.';}
+            break;
+        //operaciones
+        case 'sumar':
+            resultado.textContent='';
+            break;
+        case 'restar':
+            resultado.textContent='';
+            break;
+        case 'multiplicar':
+            resultado.textContent='';
+            break;
+        case 'dividir':
+            resultado.textContent='';
+            break;
+        //borrar
+        case 'c':
+            resultado.textContent='';
+            break;
+        case 'ce':
+            resultado.textContent='';
+            break;
+        case 'borrar':
+            let text=resultado.textContent.split('');
+            if (text.length>0){
+                text=text.slice(0,(text.length-1)).join();
+                if(text.includes (',')){
+                    text=text.replaceAll(',','');}
+            }
+            resultado.textContent=text;
             break;
     }
     
